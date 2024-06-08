5d24dabdce3aee988ca50068b40a27f36484718d
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-06-08 13:47:25 +0530

    set login page error messages

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import { Link } from 'react-router-dom'
 import PasswordInput from '../../components/Input/PasswordInput'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter a password");
+      return;
+    }
+    
+    setError("");
+  };
+  
   return (
     <>
       <Navbar/>
@@ -12,12 +34,29 @@ const Login = () => {
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
           
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h2 className="text-2xl mb-7">Login</h2>
             
-            <input placeholder="example@email.com" type="text" className="input-box" />
+            {/* email */}
+            <input 
+              placeholder="example@email.com" 
+              type="text" 
+              className="input-box" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             
-            <PasswordInput/>
+            {/* password */}
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && (
+              <p className="text-red-500 text-xs pb-1">
+                {error}
+              </p>
+            )}
 
             <button type="submit" className="btn-primary">
               Login
