7c9997ff009877a05fded775d65cb0417a78ac7a
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-06-27 05:31:50 +0530

    notes searching functionality

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import ProfileInfo from '../Crads/ProfileInfo'
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 
-const Navbar = ({ userInfo }) => {
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
     
     const [searchQuery, setSearchQuery] = useState("");
 
@@ -14,10 +14,15 @@ const Navbar = ({ userInfo }) => {
         navigate("/login");
     };
 
-    const handleSearch = () => {};
+    const handleSearch = () => {
+        if (searchQuery) {
+            onSearchNote(searchQuery);
+        }
+    };
 
     const onClearSearch = () => {
         setSearchQuery("");
+        handleClearSearch();
     };
     
     return (
